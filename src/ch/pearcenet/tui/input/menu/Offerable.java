2896da27f7fdb258ea754a6c02febb2e571b36ff
Samuel Pearce <sam@aepearce.com>
2020-02-01 21:57:16 +0100

    Rebuilt Menu System and added JUnit library

--- src/ch/pearcenet/tui/input/menu/Offerable.java
+++
@@ -1,9 +0,0 @@
-package ch.pearcenet.tui.input.menu;
-
-public abstract interface Offerable {
-	
-	public abstract void setTitle(String paramString);
-	public abstract void print();
-	public abstract int getResult();
-	
-}
