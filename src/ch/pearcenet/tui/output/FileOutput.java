d187d698dbc66a59753e59414dc8be73ca6d4975
Bismarck6502 <sam@aepearce.com>
2020-04-12 19:02:09 +0200

    Created FileOutput Class

---
+++ src/ch/pearcenet/tui/output/FileOutput.java
@@ -0,0 +1,14 @@
+package ch.pearcenet.tui.output;
+
+/**
+ * File Output Class
+ * Makes saving data to Files easier
+ *
+ * @author Samuel Pearce <sam @ aepearce>
+ * @version 2.2
+ */
+class FileOutput {
+
+
+
+}
