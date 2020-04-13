9d72e0e66aabdbdfdf8d254e0c2a024c01dc4904
Bismarck6502 <sam@aepearce.com>
2020-04-13 20:04:26 +0200

    Added some file saving methods

--- src/ch/pearcenet/tui/output/FileOutput.java
+++ src/ch/pearcenet/tui/output/FileOutput.java
@@ -1,5 +1,13 @@
 package ch.pearcenet.tui.output;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * File Output Class
  * Makes saving data to Files easier
@@ -9,6 +17,65 @@ package ch.pearcenet.tui.output;
  */
 class FileOutput {
 
+    /*
+     * Basic Data saving methods
+     */
 
+    /**
+     * Saves a string of text to a file
+     * @param filename Filename to save to
+     * @param content File contents to save
+     * @throws IOException Thrown if the content is unable to be saved to the file
+     */
+    public static void saveContent(String filename, String content) throws IOException {
+        try {
+            FileOutputStream fos = new FileOutputStream(filename);
+            PrintWriter out = new PrintWriter(fos);
+
+            out.write(content);
+
+            out.close();
+            fos.close();
+        } catch (IOException e) {
+            throw new IOException("Failed to save content to '" + filename + "'.");
+        }
+    }
+
+    /**
+     * Saves an array of bytes to a file
+     * @param filename The file to save the contents to
+     * @param content The bytes to save to the file
+     * @throws IOException Thrown if the content is unable to be saved to the file
+     */
+    public static void saveContent(String filename, byte[] content) throws IOException {
+        StringBuilder str = new StringBuilder();
+        for (byte b: content) {
+            str.append(b);
+        }
+        saveContent(filename, str.toString());
+    }
+
+    /*
+     * File Type Data saving methods
+     */
+
+    /**
+     * Saves a hashmap in a properties file
+     * @param filename The file
+     * @param content The properties to save to a file
+     * @throws IOException Thrown if the content cannot be saved
+     */
+    public static void saveProperties(String filename, HashMap<String, String> content) throws IOException {
+        StringBuilder str = new StringBuilder();
+        str.append("# '" + filename + "' created " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        for (Map.Entry entry: content.entrySet()) {
+            str.append(System.lineSeparator() + entry.getKey() + " = " + entry.getValue());
+        }
+
+        saveContent(filename, str.toString());
+    }
+
+    
 
 }
